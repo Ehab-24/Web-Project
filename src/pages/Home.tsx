f6e6305937ed305f76ba5da03f958276155e0e5d
Ehab-24 <ehabs1775@gmail.com>
2024-10-03 22:29:00 +0500

    feat: add searchbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,11 @@
+import Searchbar from "../components/Searchbar";
+
 function HomePage() {
     return (
-        <div className="text-5xl font-bold">
+        <div className="flex flex-col text-5xl font-bold">
+
+            <Searchbar className="self-center my-12" />
+
             Home Page
         </div>
     )
