39eab3b8caf56b11dc2ece7347a36f40af2746b2
Ehab-24 <ehabs1775@gmail.com>
2024-10-04 10:40:48 +0500

    feat: add footer

--- client/src/components/Logo.tsx
+++ client/src/components/Logo.tsx
@@ -1,7 +1,7 @@
 export default function Logo() {
     return (
         <div className="self-center text-xl gap-2 font-serif font-bold items-center w-40 flex justify-start">
-            <img src="../../public/logo.png" alt="Urban" className="w-8 h-full object-contain scale-y-[2]" />
+            <img src="/logo.png" alt="Urban" className="w-8 h-full object-contain scale-y-[2]" />
             <h1>Urban</h1>
         </div>
     )
